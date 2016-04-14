d1d9b6698f3ef3bfa284976db6a90ab6c20e51d9
Uriel Rodolfo Ramirez Moreno <topzihxc@gmail.com>
2016-04-14 00:52:20 -0500

    Un fragmento debe tener código

---
+++ app/src/main/java/com/platzi/fragments/fragments/fragment2.java
@@ -0,0 +1,22 @@
+package com.platzi.fragments.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.platzi.fragments.R;
+
+/**
+ * Created by Thespianartist on 14/04/16.
+ */
+public class fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+                R.layout.row2, container, false);
+    }
+}
